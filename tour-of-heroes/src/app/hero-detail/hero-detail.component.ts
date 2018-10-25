65b0d6a0b7ddeede4a30386ef0cdb26bb8bf772c
alexHooper <alexmohack@gmail.com>
2018-10-25 18:59:42 +0530

    separate hero detail portion from heroes

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from '../shared/hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+
+  @Input() hero: Hero;
 
   constructor() { }
 
