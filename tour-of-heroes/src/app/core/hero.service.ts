08b87d435642f0c41f9d4fc4dffb020776c2290e
alexHooper <alexmohack@gmail.com>
2018-10-25 19:42:29 +0530

    fetch data from service

--- tour-of-heroes/src/app/core/hero.service.ts
+++ tour-of-heroes/src/app/core/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../shared/hero'
+import { HEROES } from '../shared/mock-heroes';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+  	return HEROES;
+  }
+
 }
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../shared/hero';
-import { HEROES } from '../shared/mock-heroes';
+import { HeroService } from '../core/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,7 +17,7 @@ export class HeroesComponent implements OnInit {
   // 	name: 'Iron Man'
   // };
 
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
@@ -25,9 +25,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
