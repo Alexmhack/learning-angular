3c6b2910aae4eb83f026f4b38c89dfe9588bc6e0
alexHooper <alexmohack@gmail.com>
2018-10-25 18:46:50 +0530

    display list of heroes

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../shared/hero';
+import { HEROES } from '../shared/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,10 +12,18 @@ export class HeroesComponent implements OnInit {
 
   // hero: string = 'Windstorm';
 
-  hero: Hero = {
-  	id: 1,
-  	name: 'Iron Man'
-  };
+  // hero: Hero = {
+  // 	id: 1,
+  // 	name: 'Iron Man'
+  // };
+
+  heroes: Hero[] = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
