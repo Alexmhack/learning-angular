452cca332776af54750e1e7e83e8321188cc7cf1
alexHooper <alexmohack@gmail.com>
2018-10-26 21:09:56 +0530

    add route for hero detail

--- tour-of-heroes/src/app/app-routing.module.ts
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: '/dashboard' },
 	{ path: 'heroes', component: HeroesComponent },
+	{ path: 'hero/detail/:id', component: HeroDetailComponent },
 	{ path: 'dashboard', component: DashboardComponent },
 ]
 
--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -19,12 +19,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
