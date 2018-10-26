d91fd2003bca82069771015729cdd731b93e2e34
alexHooper <alexmohack@gmail.com>
2018-10-26 19:34:43 +0530

    generate dashboard component

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { CoreModule } from './core/core.module';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
